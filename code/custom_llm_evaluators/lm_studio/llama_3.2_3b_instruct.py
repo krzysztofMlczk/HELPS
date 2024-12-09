880b7d6757e51462dabe4bfc02b4794430190be2
krzysztofMlczk <kmalczak16@gmail.com>
2024-12-09 23:48:49 +0100

    implement custom llm (llama-3.2-3b-instruct) for lm studio usage

---
+++ code/custom_llm_evaluators/lm_studio/llama_3.2_3b_instruct.py
@@ -0,0 +1,28 @@
+from langchain_openai import OpenAI
+from deepeval.models import DeepEvalBaseLLM
+
+
+class LLama_32_3b_instruct(DeepEvalBaseLLM):
+    def __init__(self):
+        self.model = "llama-3.2-3b-instruct"
+        self.model = client = OpenAI(
+            model=self.model,
+            base_url="http://localhost:1234/v1",
+            api_key="lm-studio",
+        )
+
+    def load_model(self):
+        return self.model
+
+    def generate(self, prompt: str) -> str:
+        return self.load_model().invoke(prompt)
+
+    async def a_generate(self, prompt: str) -> str:
+        return self.generate(prompt)
+
+    def get_model_name(self):
+        return self.model
+
+
+model = LLama_32_3b_instruct()
+print(model.generate("Write me a joke"))
